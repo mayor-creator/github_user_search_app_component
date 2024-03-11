0975ceb50430ccfcc6e8a0e33e4ebcfe130e0ee4
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-03-11 11:36:03 -0400

    imported global scss variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import "../scss/global/_boilerplate.scss";
+import "../scss/global/_colors.scss";
+import "../scss/global/_fonts.scss";
+import "../scss/global/_layout.scss";
+import "../scss/global/_typography.scss";
+
 function App() {
 	return (
 		<>

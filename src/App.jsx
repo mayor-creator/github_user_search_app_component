1be98fc7301baf024047022688bd0cd246e1d6a7
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-03-12 07:06:58 -0400

    add default layout

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,36 @@ import "../scss/global/_typography.scss";
 function App() {
 	return (
 		<>
-			<h1>Git user app </h1>
+			<section>
+				<div className="wrapper">
+					<h1>Git user app </h1>
+					<header>
+						<h2>devfinder</h2>
+						<p>Light Dark </p>
+					</header>
+					<section>
+						<input
+							type="text"
+							name=""
+							id=""
+							placeholder="Search GitHub username..."
+						/>
+						<button>Search</button>
+					</section>
+					<section>
+						<h3>@octatocat</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec
+							odio. Quisque volutpat mattis eros.
+						</p>
+						<span>Joined</span>
+						<span>Repos</span>
+						<h2>8</h2>
+						<span>Followers</span>
+						<span>Following</span>
+					</section>
+				</div>
+			</section>
 		</>
 	);
 }

091cc2865478991203dc6c19bc5cca62332b426d
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-03-13 06:15:49 -0400

    add search component

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import "../scss/global/_fonts.scss";
 import "../scss/global/_layout.scss";
 import "../scss/global/_typography.scss";
 import "../scss/components/_header.scss";
+import "../scss/components/_search.scss";
 
 import Header from "./Header";
+import Search from "./Search";
 
 function App() {
 	return (
 		<>
 			<Header />
+			<Search />
 			{/*
 			<section>
 				<div className="wrapper">

f7c91f472986f7f6af845b1e5823742775914dee
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-03-16 16:18:06 -0400

    remove placeholders

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "../scss/global/_colors.scss";
 import "../scss/global/_fonts.scss";
 import "../scss/global/_layout.scss";
 import "../scss/global/_typography.scss";
+import "../scss/components/_body.scss";
 import "../scss/components/_header.scss";
 import "../scss/components/_search.scss";
 
@@ -14,37 +15,6 @@ function App() {
 		<>
 			<Header />
 			<Search />
-			{/*
-			<section>
-				<div className="wrapper">
-					<h1>Git user app </h1>
-					<header>
-						<p>Light Dark </p>
-					</header>
-					<section>
-						<input
-							type="text"
-							name=""
-							id=""
-							placeholder="Search GitHub username..."
-						/>
-						<button>Search</button>
-					</section>
-					<section>
-						<h3>@octatocat</h3>
-						<p>
-							Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec
-							odio. Quisque volutpat mattis eros.
-						</p>
-						<span>Joined</span>
-						<span>Repos</span>
-						<h2>8</h2>
-						<span>Followers</span>
-						<span>Following</span>
-					</section>
-				</div>
-			</section>
-			*/}
 		</>
 	);
 }

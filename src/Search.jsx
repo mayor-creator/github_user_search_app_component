1d01199f66ab3d6bbccef4bb7ccd7f48f8b9fb94
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-03-20 06:39:29 -0400

    handle fetch data and display it

--- src/Search.jsx
+++ src/Search.jsx
@@ -25,6 +25,111 @@ function Search() {
 		}
 	};
 
+	// handling data after handleClickFetchAPI function
+	const fetchData = (data) => {
+		if (data) {
+			return (
+				<>
+					<section className="main_information">
+						<div
+							key={data.id}
+							className="information_container">
+							<img
+								className="avatar"
+								src={data.avatar_url}
+								alt="profile picture"
+								height={117}
+								width={117}
+							/>
+							<div className="profile_container">
+								<div className="profile_info_container">
+									<div className="info_item">
+										<h1 className="profile_name">{data.name}</h1>
+										<p className="profile_login">
+											<a
+												href={`${data.html_url}`}
+												target="_blank">
+												@{data.login}
+											</a>
+										</p>
+									</div>
+									<div className="info_item">
+										<p className="profile_date">Joined {data.created_at}</p>
+									</div>
+								</div>
+								<div className="bio_container">
+									<p className="bio_information">{data.bio}</p>
+									<div className="github_information">
+										<div className="github_items">
+											<span className="github">Repos</span>
+											<h2 className="github_number">{data.public_repos}</h2>
+										</div>
+										<div className="github_items">
+											<span className="github">Followers</span>
+											<h2 className="github_number">{data.followers}</h2>
+										</div>
+										<div className="github_items">
+											<span className="github">Following</span>
+											<h2 className="github_number">{data.following}</h2>
+										</div>
+									</div>
+									<div className="github_social">
+										<div className="social_items">
+											<img
+												src={locationImage}
+												alt="location icon"
+											/>
+											<p>
+												{data.location != null
+													? data.location
+													: "Not Available"}
+											</p>
+										</div>
+										<div className="social_items">
+											<img
+												src={twitterImage}
+												alt="twitter icon"
+											/>
+											<p>
+												{data.twitter_username != null
+													? data.twitter_username
+													: "Not Available"}
+											</p>
+										</div>
+										<div className="social_items">
+											<img
+												src={websiteImage}
+												alt="blog icon"
+											/>
+											<p>
+												<a
+													href={`http://${data.blog}`}
+													target="_blank">
+													{data.blog != "" ? data.blog : "Not Available "}
+												</a>
+											</p>
+										</div>
+										<div className="social_items">
+											<img
+												src={companyImage}
+												alt="company icon"
+											/>
+											<p>
+												{data.company != null ? data.company : "Not Available"}
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
+				</>
+			);
+		} else {
+			return null;
+		}
+	};
+
 	return (
 		<>
 			<main className="main">
@@ -55,7 +160,7 @@ function Search() {
 							Search
 						</button>
 					</section>
-
+					{/*  
 					<section className="main_information">
 						<div className="information_container">
 							<img
@@ -134,6 +239,9 @@ function Search() {
 							</div>
 						</div>
 					</section>
+         */}
+
+					{fetchData(data)}
 				</div>
 			</main>
 		</>

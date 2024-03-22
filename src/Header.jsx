75dfc2f218cf892b7c4564dcba317542bd69e93a
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-03-22 11:32:03 -0400

    add class name to button

--- src/Header.jsx
+++ src/Header.jsx
@@ -9,11 +9,11 @@ function Header() {
 			<header className={`${theme} header`}>
 				<div className="wrapper">
 					<div className="header_container">
-						<h2 className="header_logo">devfinder</h2>
+						<h2 className="header_logo devfinder">devfinder</h2>
 						<button
 							onClick={toggleTheme}
 							type="image"
-							className="header_button">
+							className="header_button button">
 							{theme}
 						</button>
 					</div>
